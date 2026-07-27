64f3760bbc09951b16e7adc374581afec448e5e5
Nirmal Kumar <106423938+Nirmal970@users.noreply.github.com>
2026-07-27 12:02:20 +0530

    refactor

--- airflow/healthcare_pipeline_dag.py
+++ airflow/healthcare_pipeline_dag.py
@@ -18,7 +18,7 @@ dag_combined = DAG(
     'dag_combined',
     default_args=default_args,
     description='Combined DAG for Spark Jobs and EMR Steps',
-    schedule_interval='@daily',  # Adjust the schedule interval as needed
+    schedule_interval='@daily',  
 )
 
 # EMR Spark Step Configuration
@@ -27,14 +27,14 @@ emr_spark_step = {
     'ActionOnFailure': 'CONTINUE',
     'HadoopJarStep': {
         'Jar': 'command-runner.jar',
-        'Args': ['spark-submit', '/path/to/your/app_s3_to_ec2.jar']  # Replace with the actual path
+        'Args': ['spark-submit', '/home/hadoop/task_t1.jar']
     }
 }
 
 # EMR Add Steps Operator
 emr_step_adder = EmrAddStepsOperator(
     task_id='add_emr_spark_step',
-    job_flow_id='j-2A5H6N96VNGSN',  # Replace with your EMR cluster ID
+    job_flow_id='j-2A5H6N96VNGSN', 
     aws_conn_id='aws_default',
     steps=[emr_spark_step],
     dag=dag_combined
@@ -43,30 +43,28 @@ emr_step_adder = EmrAddStepsOperator(
 # EMR Step Sensor
 emr_step_checker = EmrStepSensor(
     task_id='watch_emr_spark_step',
-    job_flow_id='j-2A5H6N96VNGSN',  # Replace with your EMR cluster ID
+    job_flow_id='j-2A5H6N96VNGSN',  
     step_id="{{ task_instance.xcom_pull(task_ids='add_emr_spark_step')['StepIds'][0] }}",
     aws_conn_id='aws_default',
     dag=dag_combined
 )
 
-# Bash Operators for Local Spark Jobs
 t1 = BashOperator(
     task_id='t1_s3_to_ec2',
-    bash_command='spark-submit /path/to/your/app_s3_to_ec2.jar',  # Replace with the actual path
+    bash_command='spark-submit /home/hadoop/task_t1.jar',
     dag=dag_combined,
 )
 
 t2 = BashOperator(
     task_id='t2_transform_and_save',
-    bash_command='spark-submit /path/to/your/app_transform_and_save.jar',  # Replace with the actual path
+    bash_command='spark-submit /home/hadoop/task_t2.jar',
     dag=dag_combined,
 )
 
 t3 = BashOperator(
     task_id='t3_export_to_s3',
-    bash_command='spark-submit /path/to/your/app_export_to_s3.jar',  # Replace with the actual path
+    bash_command='spark-submit /home/hadoop/task_t3.jar',
     dag=dag_combined,
 )
 
-# Set up dependencies
 t1 >> t2 >> t3 >> emr_step_adder >> emr_step_checker
